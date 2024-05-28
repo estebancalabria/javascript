08c5843389c6512849568c6d647adedaf6503860
Esteban Calabria <esteban.calabria@gmail.com>
2024-05-28 12:55:17 -0300

    clase-dos

--- 71396-Ma-MaVie/clase-dos/script_despues.js
+++ 71396-Ma-MaVie/clase-dos/script_despues.js
@@ -1,10 +1,16 @@
 
 //Esto si se ejecuta
 //document.getElementById("titulo").innerHTML = "Despues: Titulo de la pagina desde JS"
+function empiezaConMayuscula(str) {
+    return /^[A-Z][a-z]*$/.test(str);
+}
+
+
 function saludar() {
     let txtNombre = document.querySelector("#txtNombre");
     Swal.fire(`Hola ${txtNombre.value}!`);
 }
+
 
 let btnSaludar = document.querySelector("#btnSaludar");
 btnSaludar.addEventListener("click", function(){
@@ -17,9 +23,23 @@ txtNombre.addEventListener("keypress", function(e){
     console.log(e.constructor.name);
     console.dir(e);
     console.log("------------------------------");*/
-    if(e.key === "Enter"){
+
+    /*if (!txtNombre.value.match(/^[A-Z][a-z]*$/)){
+        let txtErrorNombre = document.querySelector("#txtErrorNombre");
+        txtErrorNombre.style.display = "block";
+        txtErrorNombre.innerHTML = "Error: El nombre debe empezar con una letra mayuscula y tener al menos 2 caracteres";
+    }else{
+        txtErrorNombre.style.display = "none";
+    }*/
+
+
+    txtErrorNombre.style.display = (!empiezaConMayuscula(txtNombre.value)) ? "block" : "none";
+    txtErrorNombre.innerHTML = "Error: El nombre debe empezar con una letra mayuscula y tener al menos 2 caracteres";
+
+    /*if(e.key === "Enter"){
         saludar();
-    }
+    }*/
+    (e.key === "Enter") && saludar();
 
     //     = asignación
     // == compara contenido
---
+++ links.md
@@ -0,0 +1,44 @@
+
+# Repo
+
+* Javascript Algoritmos y Estructuras de Datos
+> https://github.com/trekhleb/javascript-algorithms
+
+* Repo del Libro : You dont know Javascript Yet
+> https://github.com/getify/You-Dont-Know-JS
+
+* Entrevistas de trabajo asombrosas
+> https://github.com/DopplerHQ/awesome-interview-questions
+
+* Airbnb Javascript Style Guide
+> https://github.com/airbnb/javascript
+
+* Clean Code Javascript
+> https://github.com/ryanmcdermott/clean-code-javascript
+
+* 33 Conceptos de Javascript que todo desarrollador deberia saber
+> https://github.com/leonardomso/33-js-concepts
+
+* WTF Javascript
+> https://github.com/denysdovhan/wtfjs
+
+# Librerias
+
+* LIGHTBOX
+> https://lokeshdhakar.com/projects/lightbox2/
+
+* Sweetalert2
+> https://sweetalert2.github.io/
+
+# Extensiones VSCode
+
+* Console Ninja
+* EsList
+* Cody :)
+* LiveServer
+* LiveShare
+
+# Atajos VsCode
+
+* Comentar Varias lineas : [Ctrl]+[/]
+* Shift+Alt+a  --> comenta todo un bloque de código previamente seleccionado
